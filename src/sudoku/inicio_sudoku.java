f37d2cbbe2bfe3bd67d9fc47d5e206b916d61eac
Poniente <Poniente@Poniente-PC>
2018-03-09 10:45:41 +0100

    creacción del esqueleto del sudoku

---
+++ src/sudoku/inicio_sudoku.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+/**
+ *Esta clase es la clase principal donde se inicia el sudoku
+ * @author Poniente
+ */
+public class inicio_sudoku {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
