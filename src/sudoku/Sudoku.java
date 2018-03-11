29c85f9bfbb34c64ac743c4421232f58ca31fadb
Poniente <Poniente@Poniente-PC>
2018-03-11 10:02:20 +0100

    implementacion del modulo inicializar finalizada

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -5,6 +5,9 @@
  */
 package sudoku;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *Esta es la clase sudoku 
  * @author Poniente
@@ -13,15 +16,63 @@ package sudoku;
 public class Sudoku 
 {
     // Variables privadas de la clase
-
+    //array donde se almacenaran los numeros del sudoku
+    private ArrayList<ArrayList<Integer>> numeros_sudoku;
+    
+    /**
+     * contructor de la clase sudoku
+     */
     public Sudoku() 
     {
-        
+        inicializar();
     }
 
+    /**
+     * Esta clase crea un sudoku de manera aleatoria 
+     */
     public void inicializar() 
     {
-
+        ArrayList<ArrayList<Integer>> solucion = new ArrayList();
+        ArrayList<Integer> numeros = new ArrayList();
+        Random aleatorio = new Random();
+        boolean cent = true;
+        int contador = 0;
+        /*
+        *creamos todos los espacios del sudoku para posteriormente introducir
+        *numeros aleatorios 
+        */
+        for (int i = 0; i < 27 ; i++) {
+            ArrayList<Integer> insercion = new ArrayList();
+            for (int j = 0; j < 3; j++) {
+                insercion.add(0);
+            }
+            solucion.add(insercion);
+        }
+        /*
+        *añadimos los numeros generados de manera aleatorioa al sudoku y 
+        *cumpliendo las normas de no repetir numeros donde corresponda
+        *.set(aleatorio.nextInt(solucion.size()-1),numeros.get(i));
+        */
+        for (int i = 0; i < 31 ; i++) {
+            /*
+            *cogemos una fila aleatorioa y generamos un numero aleatorio 
+            *para insertar
+            */
+            cent = true;
+            while(cent){
+                int num_fila = aleatorio.nextInt(3);
+                int num_columna = aleatorio.nextInt(solucion.size()-1);
+                int num_insertar = aleatorio.nextInt(9)+1;
+                if(!comprobarFila(num_columna,num_insertar)){
+                    if (!comprobarColumna(num_columna,num_insertar)) {
+                        if (!comprobarCuadrante(num_fila, num_columna,num_insertar)) {
+                            solucion.get(num_columna).set(num_fila, num_insertar);
+                            cent = false;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     @Override
--- src/sudoku/inicio_sudoku.java
+++ src/sudoku/inicio_sudoku.java
@@ -15,7 +15,7 @@ public class inicio_sudoku {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
